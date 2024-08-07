ba6f3172a135ecdf516c79a949de1fa38a9fee52
Bediz Türkyılmaz <149793595+Bedizz@users.noreply.github.com>
2024-08-07 16:48:13 +0200

    Delete frontend directory

--- frontend/src/atoms/authAtoms.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const authScreenAtom = atom({
-    key: 'authScreen',
-    default: 'login',
-});
--- frontend/src/atoms/userAtoms.js
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-
-export const userAtom = atom({
-    key: 'userAtom',
-    default: JSON.parse(localStorage.getItem('user-threads'))
-
-})
